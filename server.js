912e1779357dc358d4982f897e6c94d16b0e6fe0
Jason <kangja1996@gmail.com>
2021-06-27 20:28:10 -0400

    finished

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const User = require("./users");
+
+mongoose.connect("mongodb://localhost/pagination", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.once("open", async () => {
+  if ((await User.countDocuments().exec()) > 0) return;
+  Promise.all([
+    User.create({ name: "User 1" }),
+    User.create({ name: "User 2" }),
+    User.create({ name: "User 3" }),
+    User.create({ name: "User 4" }),
+    User.create({ name: "User 5" }),
+    User.create({ name: "User 6" }),
+    User.create({ name: "User 7" }),
+    User.create({ name: "User 8" }),
+    User.create({ name: "User 9" }),
+    User.create({ name: "User 10" }),
+    User.create({ name: "User 11" }),
+    User.create({ name: "User 12" }),
+  ]).then(() => console.log("Added users"));
+});
 
 // const users = [
 //   { id: 1, name: "User 1" },
@@ -37,12 +63,12 @@ const app = express();
 //   res.json(res.paginatedResults);
 // });
 
-app.get("/users", paginatedResults(users), (req, res) => {
+app.get("/users", paginatedResults(User), (req, res) => {
   res.json(res.paginatedResults);
 });
 
 function paginatedResults(model) {
-  return (req, res, next) => {
+  return async (req, res, next) => {
     const page = parseInt(req.query.page);
     const limit = parseInt(req.query.limit);
 
@@ -51,7 +77,7 @@ function paginatedResults(model) {
 
     const results = {};
 
-    if (endIndex < model.length) {
+    if (endIndex < (await model.countDocuments().exec())) {
       results.next = {
         page: page + 1,
         limit: limit,
@@ -64,11 +90,13 @@ function paginatedResults(model) {
         limit: limit,
       };
     }
-
-    results.results = model.slice(startIndex, endIndex);
-
-    res.paginatedResults = results;
-    next();
+    try {
+      results.results = await model.find().limit(limit).skip(startIndex).exec();
+      res.paginatedResults = results;
+      next();
+    } catch (e) {
+      res.status(500).json({ message: e.msessage });
+    }
   };
 }
 
